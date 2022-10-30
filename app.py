7c43bdf451e8d29aa92b4ecf2bf4f80527b46b62
av1998av <av1998av@gmail.com>
2022-10-31 01:18:24 +0530

    adding cors support

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import json
 from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
 import requests
 
 app = Flask(__name__)
+cors = CORS(app)
 
 player_team_associations_data = [
     {'teamId': 0, 'playerId': 9782},
